8cbdad8715ca921fd5ea483156b5d7f7fad8aa53
Rp <61639693+Hide349@users.noreply.github.com>
2024-04-18 02:30:25 +0000

    meu-portifólio

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ruan Pablo Furtado Oliveira",
+  title: "Cientista da computação",
+  email: "ruan@pablooliveira20@gmail.com",
+  gitHub: "Hide349",
+  instagram: "ruan_furtad",
+  linkedIn: "****",
   medium: "",
-  twitter: "microsoft",
+  twitter: "*****",
   youTube: "Code",
 };
 
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,22 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Projeto de circuitos digitais 🎉",
     description:
       "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    url: "https://github.com/Hide349/Guess-The-Number-Extreme",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Site com login em php",
     description:
       "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    url: "https://github.com/Hide349/Login-e-Cadastro",
   },
   {
-    title: "My Resume Site",
+    title: "Flappy bird com flutter",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    url: "https://github.com/Hide349/flappy_bird",
   },
   {
     title: "GitHub Codespaces and github.dev",
